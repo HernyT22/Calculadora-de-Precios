916ab446a450db980ae91b4f906e7538ab489da7
HGuevara <113396996+HernyT22@users.noreply.github.com>
2026-02-05 18:20:35 -0300

    Add files via upload

--- mains.js
+++ mains.js
@@ -1,7 +1,7 @@
 let content = document.getElementById('container');
 document.getElementById('btn-calcular').addEventListener('click', () => {
    
-
+//Funciones Importantes
 function applyTaxes(costo, impuestos){
     return costo * (1 + (impuestos / 100));
 }
@@ -9,26 +9,33 @@ function applyTaxes(costo, impuestos){
 function priceProfit(costo, margen){
     return (costo / (100 - margen) * 100);
 }
+function roundPrice(value){
+    return Math.ceil(value / 100) * 100;
+};
+
+//Constantes
 
   let COST = parseFloat(document.getElementById('costo').value);
   const PORCENT = parseFloat(document.getElementById('porcentaje').value);
   const ADIT = parseFloat(document.getElementById('adicionales').value) || 0;
   const TAX = parseFloat(document.getElementById('impuestos').value) || 0;
 
-COST = applyTaxes(COST, TAX);
+  //Aplicar impuestos
+    COST = applyTaxes(COST, TAX);
+  //Calcular Ganancia +  adicionales
+    let salePrice = priceProfit(COST, PORCENT) + ADIT;
+  //Redondear Numero por Centenas
+    salePrice = roundPrice(salePrice);
 
-let salePrice = priceProfit(COST, PORCENT) + ADIT;
+ //Imprimir resultado
+    let resultBox = document.getElementById('cont-salePrice');
 
- console.log(salePrice);
+    if(!resultBox){
+        resultBox = document.createElement('div');
+        resultBox.id = 'cont-salePrice';
+        content.appendChild(resultBox);
+    }
 
-
- let resultBox = document.getElementById('cont-salePrice');
-
- if(!resultBox){
-    resultBox = document.createElement('div');
-    resultBox.id = 'cont-salePrice';
-    content.appendChild(resultBox);
- }
- resultBox.innerHTML = `<h1 class="title-PV">Precio de Venta</h1>
-                        <h2>$${salePrice.toFixed(2)}</h2>`;
+    resultBox.innerHTML = `<h1 class="title-PV">Precio de Venta</h1>
+                            <h2>$${salePrice.toFixed(2)}</h2>`;
 });
